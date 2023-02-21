071ff2574687d499bbdd19c847fe6c0cd18ecddf
Sumanth <93238666+Sumanth1680@users.noreply.github.com>
2023-02-21 19:52:16 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
+import gtts
 from newsapi import NewsApiClient
 import pycountry
-from urllib.request import urlopen
-import re
-from bs4 import BeautifulSoup
-import requests
-
+from newsfetch.news import newspaper
+import os
+from summarizer import Summarizer
+from playsound import playsound
 newsapi = NewsApiClient(api_key='0c5239181cb5425782763385b08f7f09')
-input_country = input("Country: ")
+input_country = "India"
 input_countries = [f'{input_country.strip()}']
 countries = {}
 for country in pycountry.countries:
@@ -27,7 +27,7 @@ top_headlines = newsapi.get_top_headlines(
 
 # fetch the top news under that category
 Headlines = top_headlines['articles']
-print(Headlines)
+# print(Headlines)
 lst_articles = []
 i = 0
 # now we will display the that news with a good readability for user
@@ -47,11 +47,32 @@ if Headlines:
             print(str(i) + ".",
                   f"{articles['title'][-b + 1:]} News: {articles['title'][:-b - 2]}.")
 else:
-    print(f"Sorry no articles found for {input_country}, Something Wrong!!!")
+    print(f"Sorry no articles found for {input_country}, Something is Wrong!!!")
 print('\n')
-print(lst_articles)
-# choice = int(input('Select the article you want to know about : '))
-# if choice <= len(lst_articles):
-#
-# else:
-#     print('The article number You have selected is not available. Please try again later!!')
+# print(lst_articles)
+choice = int(input('Select the article you want to know about : '))
+if choice <= len(lst_articles):
+    news = newspaper(lst_articles[choice - 1])
+    try:
+        os.remove("news_article.txt")
+        file = open("news_article.txt", "x")
+        file.close()
+        file = open("news_article.txt", "a")
+        file.write(news.article)
+        file.close()
+    except:
+        file = open("news_article.txt", "x")
+        file.close()
+        file = open("news_article.txt", "a")
+        file.write(news.article)
+        file.close()
+    f = open("news_article.txt","r")
+    bert_model = Summarizer()
+    bert_summary = ''.join(bert_model(f.read(), min_length=60))
+    print(bert_summary)
+    tts = gtts.gTTS(bert_summary, lang="en")
+    tts.save("G:/Projects/Text-Summerization/my-news.mp3")
+    playsound("G:/Projects/Text-Summerization/my-news.mp3")
+    os.remove("G:/Projects/Text-Summerization/my-news.mp3")
+else:
+    print('The article number You have selected is not available. Please try again later!!')
